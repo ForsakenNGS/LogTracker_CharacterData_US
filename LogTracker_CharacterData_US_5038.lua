9ef868172c0324407dab565c14f8c83c1465aa60
Jens N <jens.n@godlike.biz>
2022-10-10 03:05:06 +0200

    Addon data update 2022-10-10 03:05

--- LogTracker_CharacterData_US_5038.lua
+++ LogTracker_CharacterData_US_5038.lua
@@ -3,5 +3,5 @@ if not (GetRealmName() == 'Westfall') then
   return
 end
 LogTracker_CharacterData_US['Westfall'] = {
-['Cozo']={70,'Alliance',10,1665363783,{['0-1015']={0,0,{},''}}},
+['Cozo']={70,'Alliance',10,1665363903,{['0-1015']={0,0,{},''}}},
 };
